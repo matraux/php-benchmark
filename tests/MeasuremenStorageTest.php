8d1ec5a24798223b18042764a0ec2dd3a34ec32f
Daniel Žůrek <info@danielzurek.cz>
2025-07-25 19:48:10 +0200

    Feat: Tests

---
+++ tests/MeasuremenStorageTest.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types = 1);
+
+namespace Matraux\PhpBenchmarkTest;
+
+use LogicException;
+use Matraux\PhpBenchmark\Measurement\Measurement;
+use Tester\TestCase;
+use Matraux\PhpBenchmark\Measurement\MeasurementStorage;
+use Tester\Assert;
+
+require_once __DIR__ . '/Bootstrap.php';
+
+Bootstrap::tester();
+
+/**
+ * @testCase
+ */
+final class MeasuremenStorageTest extends TestCase
+{
+
+	public function testSingleton(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		$a = MeasurementStorage::create();
+		$b = MeasurementStorage::create();
+
+		Assert::equal($a, $b);
+	}
+
+	public function testAdd(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		$measurement = Measurement::create('', 0, [0], 0);
+
+		Assert::noError(function()use($measurement){
+			MeasurementStorage::add($measurement);
+			MeasurementStorage::add($measurement);
+		});
+	}
+
+	public function testCountable(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		$storage = MeasurementStorage::create();
+
+		Assert::count(0, $storage);
+	}
+
+	public function testIterator(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		Assert::noError(function(){
+			foreach(MeasurementStorage::create() as $measurement) {
+
+			}
+		});
+
+		Assert::noError(function(){
+			iterator_to_array(MeasurementStorage::create());
+		});
+	}
+
+	public function testClone(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		Assert::error(function(){
+			$clone = clone MeasurementStorage::create();
+		}, LogicException::class);
+	}
+
+}
+
+new MeasuremenStorageTest()->run();
---
+++ tests/MeasuremenTest.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types = 1);
+
+namespace Matraux\PhpBenchmarkTest;
+
+use Matraux\PhpBenchmark\Measurement\Measurement;
+use Tester\TestCase;
+use Tester\Assert;
+
+require_once __DIR__ . '/Bootstrap.php';
+
+Bootstrap::tester();
+
+/**
+ * @testCase
+ */
+final class MeasuremenTest extends TestCase
+{
+
+	public function testProperties(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		$measurement = Measurement::create(
+			label: 'Label',
+			counter: 1,
+			times: [0, 1.0, 2, 3.0, 4, 5.0],
+			memory: 10
+		);
+
+		Assert::equal($measurement->average, 2.5);
+		Assert::equal($measurement->min, 0.0);
+		Assert::equal($measurement->max, 5.0);
+		Assert::equal($measurement->total, 15.0);
+		Assert::equal($measurement->multiplier, 6);
+		Assert::equal(round($measurement->deviation, 4), 1.7078);
+
+		Assert::equal($measurement->label, 'Label');
+		Assert::equal($measurement->counter, 1);
+		Assert::equal($measurement->times, [0, 1.0, 2, 3.0, 4, 5.0]);
+		Assert::equal($measurement->memory, 10);
+		Assert::equal($measurement->file, null);
+		Assert::equal($measurement->line, null);
+	}
+
+	public function testJsonSerialize(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		$measurement = Measurement::create(
+			label: 'Label',
+			counter: 1,
+			times: [0],
+			memory: 0
+		);
+
+		Assert::noError(function()use($measurement){
+			json_encode($measurement);
+		});
+	}
+
+	public function testStringable(): void
+	{
+		Bootstrap::purgeTemp(__FUNCTION__);
+
+		$measurement = Measurement::create(
+			label: 'Label',
+			counter: 1,
+			times: [0],
+			memory: 0
+		);
+
+		Assert::noError(function()use($measurement){
+			(string) $measurement;
+		});
+	}
+
+}
+
+new MeasuremenTest()->run();
